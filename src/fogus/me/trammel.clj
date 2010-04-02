6996d20d4f861ab8fab82e0fea72985884396e92
Fogus <fogusm@tsubasa.tena-sda.org>
2010-04-02 10:31:36 -0400

    first implementation.

--- src/fogus/me/trammel.clj
+++ src/fogus/me/trammel.clj
@@ -1,5 +1,28 @@
 (ns fogus.me.trammel
   (:require '[clojure.test :as test]))
 
-(defmacro defcontract [& specs]
-  nil)
+(defn- build-contract [c]
+  (let [args (first c)]
+    (list
+     (into '[f] args)
+     (apply merge
+            (for [con (rest c)]            
+              (cond (= (first con) 'require)
+                    (assoc {} :pre (vec (rest con)))
+                    (= (first con) 'ensure)
+                    (assoc {} :post (vec (rest con)))
+                    :else (throw (Exception. (str "Unknown tag " (first con)))))))
+     (list* 'f args)))) 
+
+(defn- collect-bodies [forms]
+  (for [form (partition 3 forms)]
+    (build-contract form)))
+
+(defmacro defcontract [& forms]
+  (let [name (if (symbol? (first forms))
+               (first forms) 
+               nil)
+        body (collect-bodies (if name
+                               (rest forms)
+                               forms))]
+    (list* 'fn name body)))
