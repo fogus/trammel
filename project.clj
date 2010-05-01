c13dc40ffa21b173c25fc26b466abb92efe28321
fogus <mefogus@gmail.com>
2010-04-30 20:25:30 -0400

    added leiningen build file

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject trammel "0.0.1"
+  :description "A library providing contracts programming for Clojure."
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]]
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
+                     [leiningen/lein-swank "1.1.0"]
+                     [jline "0.9.94"]])
