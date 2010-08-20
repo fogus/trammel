800e587f4473492967799293bc7fe71244dfaceb
fogus <mefogus@gmail.com>
2010-08-20 11:26:03 -0400

    Upgraded to Clojure 1.2

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject trammel "0.2.0"
   :description "A library providing contracts programming for Clojure."
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.2.0"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
                      [jline "0.9.94"]])
