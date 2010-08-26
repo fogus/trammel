12e3b3a1c5653cd94a1edcc01f94102a8c9e07df
fogus <mefogus@gmail.com>
2010-08-25 21:21:46 -0400

    Added the new contracts macro, looks different, but works the same as the old

--- src/fogus/me/trammel.clj
+++ src/fogus/me/trammel.clj
@@ -84,78 +84,6 @@
           pre-post-map
           (list* 'f args))))
 
-(defn build-contract 
-  [arity-map]
-  (let [sig (first (keys arity-map))]
-    (list 
-     (into '[f] sig)
-     (build-constraints-map (first (vals arity-map)))
-     (list* 'f sig))))
-
-(defn collect-bodies
-  "Where the magic happens.  Takes a list representing the idependent constraints for 
-   an unspecified function of the form:
-
-    ([x] :requires (foo x) :ensures (bar %) 
-     [x y] :requires (baz x) (quux y) :ensures (blip %))
-
-   Taking this form `collect-bodies` then partitions it in such a way as to identify
-   the arity-based canstraints and pass each on to `build-contract` which then 
-   returns the HOF body for each arity.
-  "
-  [arity-maps]
-  (for [amap arity-maps]
-    (build-contract amap)))
-
-
-(defn build-forms-map
-  "Works similarly to the `collect-bodies` function *except* for two differences:
-   1. Recognizes and processes a `:body` element
-   2. Requires that multi-arity functions *must* be enclosed in sublists similar to 
-      Clojure's default `fn` form.
-
-   The reason for the former is that the `:body` element refers to the body of the
-   generated function.  Without arity-sepcific body sublists there would be no way to 
-   determine when a body ends and another arity constraint specification begins.  This
-   should be no problem since Clojurist are already accustomed to the multi-arity `fn` 
-   form.  
-
-   This function expects a list of the form:
-
-    ([x] :requires (foo x) (bar n) :ensures (baz %) :body (println x) (frob x))
-
-   or
-
-    (([x] :requires (foo x) :ensures (baz %) :body (frob x)) 
-     ([x y] :requires (foo x y) :ensures (baz %) :body (frob x y)))
-
-   And will return a map keyed by arity vector of the form:
-
-    {[x] {(:requires) ((foo x)), (:body) ((frob x)), (:ensures) ((baz %))}}
-
-   Which can then be used to build a real function definition by looking up the
-   constraints and body for each arity.
-  "
-  [forms]
-  (for [[[args] & c] (map #(partition-by #{:requires :ensures :body} %)
-                       (if (vector? (first forms)) 
-                         (list forms) 
-                         forms))]
-    {args (let [m (apply array-map c)
-                ks (map first (keys m))
-                vs (vals m)]
-            (zipmap ks (map (fn [k vs]
-                              (map (fn [v]
-                                     (if (symbol? v)
-                                       (cond
-                                        (= k :requires) (apply list v args)
-                                        (= k :ensures)  (apply list v '[%])
-                                        :default        v)
-                                       v))
-                                   vs))
-                            ks
-                            vs)))}))
-
 (defmacro contract
   "The base contract form returning a higher-order function that can then be partially
    applied to an existing function to 'apply' a contract.  Take for example a simple
@@ -164,19 +92,9 @@
    
     (def doubler-contract
        (contract doubler
-         [x]
-         :requires
-         (number? x)
-    
-         :ensures
-         (= (* 2 x) %)
-    
-         [x y]
-         :requires
-         (every? number? [x y])
-           
-         :ensures
-         (= (* 2 (+ x y)) %)))
+         [x] [number? => (= (* 2 x) %)]
+
+         [x y] [(every? number? [x y]) => (= (* 2 (+ x y)) %)]))
 
    You can then partially apply this contract with an existing function:
 
@@ -193,27 +111,23 @@
 
    Similar results would occur for the 2-arity versions of `doubler` and `bad-doubler`.
 
-   While it's fine to use `partial` directly, it's better to use the `with-constraints` macro
+   While it's fine to use `partial` directly, it's better to use the `with-constraints` function
    found in this same library.
 
    If you're so inclined, you can inspect the terms of the contract via its metadata, keyed on
    the keyword `:constraints`.
   "
-  [& forms]
-  (let [name (when (symbol? (first forms))
-               (first forms))
-        arity-maps (build-forms-map
-                    (map #(mapcat identity %) 
-                         (partition 2
-                                    (partition-by
-                                     vector?
-                                     (if name
-                                       (rest forms)
-                                       forms)))))]
+  [name docstring & constraints]
+  (println name docstring constraints)
+  (let [raw-cnstr   (->> (partition-by vector? constraints) 
+                         (partition 2))
+        arity-cnstr (for [[[a] c] raw-cnstr]
+                      (build-constraints-map a c))
+        fn-arities  (for [b arity-cnstr]
+                      (build-contract b))]
     (list `with-meta 
-          (list* `fn (if name name (gensym))
-                 (collect-bodies arity-maps))
-          `{:constraints (into {} '~arity-maps)})))
+          (list* `fn name fn-arities)
+          `{:constraints (into {} '~arity-cnstr)})))
 
 (defmacro with-constraints
   "Takes a target function and a number of contracts and returns a function with the contracts
