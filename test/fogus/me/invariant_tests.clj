cf7b146ee0acbdbedd19878975d77e228c42bb77
fogus <mefogus@gmail.com>
2010-09-16 21:30:47 -0400

    first tests for record invars

---
+++ test/fogus/me/invariant_tests.clj
@@ -0,0 +1,25 @@
+;;; invariant_tests.clj -- Contracts programming library for Clojure
+
+;; by Michael Fogus - http://fogus.me/fun/
+;; Sept 16, 2010
+
+;; Copyright (c) Michael Fogus, 2010. All rights reserved.  The use
+;; and distribution terms for this software are covered by the Eclipse
+;; Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;; which can be found in the file COPYING the root of this
+;; distribution.  By using this software in any fashion, you are
+;; agreeing to be bound by the terms of this license.  You must not
+;; remove this notice, or any other, from this software.
+
+(ns fogus.me.invariant-tests
+  (:use [fogus.me.trammel :only [defconstrainedrecord]])
+  (:use [clojure.test :only [deftest is]]))
+
+(defconstrainedrecord Foo [a 1 b 2]
+  [(every? number? [a b])]
+  Object
+  (toString [this] (str "record Foo has " a " and " b)))
+
+(deftest test-constrained-record
+  (is (= (:a (new-Foo :a 42 :b 108)) 42))
+  (is (= (:b (new-Foo :a 42 :b 108)) 108)))
