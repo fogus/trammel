3563a9ce494e7708dd2227b98204052e447caa9a
fogus <mefogus@gmail.com>
2011-01-27 13:13:18 -0500

    Testing cnstr records using pre/post maps

--- test/fogus/me/invariant_tests.clj
+++ test/fogus/me/invariant_tests.clj
@@ -21,7 +21,7 @@
   Object
   (toString [this] (str "record Foo has " a " and " b)))
 
-(deftest test-constrained-record
+(deftest test-constrained-record-with-vector-spec
   (is (= (:a (new-Foo)) 1))
   (is (= (:b (new-Foo)) 2))
   (is (= (:a (new-Foo :a 42)) 42))
@@ -33,3 +33,23 @@
   (is (= (:c (new-Foo :a 42 :b 108 :c 36)) 36))
   (is (thrown? Error (new-Foo :a :b)))
   (is (thrown? Error (new-Foo :a 42 :b nil))))
+
+;; testing default clojure pre/post maps
+
+(defconstrainedrecord Bar [a 1 b 2]
+  {:pre [(every? number? [a b])]}
+  Object
+  (toString [this] (str "record Bar has " a " and " b)))
+
+(deftest test-constrained-record-with-map-spec
+  (is (= (:a (new-Bar)) 1))
+  (is (= (:b (new-Bar)) 2))
+  (is (= (:a (new-Bar :a 42)) 42))
+  (is (= (:b (new-Bar :b 108)) 108))
+  (is (= (:a (new-Bar :a 42 :b 108)) 42))
+  (is (= (:b (new-Bar :a 42 :b 108)) 108))
+  (is (= (:a (new-Bar :a 42 :b 108 :c 36)) 42))
+  (is (= (:b (new-Bar :a 42 :b 108 :c 36)) 108))
+  (is (= (:c (new-Bar :a 42 :b 108 :c 36)) 36))
+  (is (thrown? Error (new-Bar :a :b)))
+  (is (thrown? Error (new-Bar :a 42 :b nil))))
