2d232d75f0e39dd771259de4ddfbdb8279b5e80c
fogus <mefogus@gmail.com>
2011-02-01 08:00:05 -0500

    Added whitelist and added docstring to in

--- src/trammel/factors.clj
+++ src/trammel/factors.clj
@@ -1,5 +1,6 @@
 (ns trammel.factors
-  "Provides some common argument checkers and factorings.")
+  "Provides some common argument checkers and factorings."
+  (:require [clojure.set :as set]))
 
 ;; # constraint functions and multimethods
 
@@ -8,7 +9,24 @@
 (def all-negative? #(and (apply all-numbers? %&) (every? (complement pos?) %&)))
 (defn anything [& _] true)
 
-(defn in [e & args] (some #{e} (mapcat #(if (vector? %) 
-                                          (apply range %) 
-                                          [%]) 
-                                       args)))
+(defn in
+  "Takes an item and determines if it falls in the listed args.  This can be
+   used most effectively for numbers since any numbers in a vector represent
+   a range of values determined by the same arguments as given to `range`."
+  [e & args]
+  (some #{e}
+        (mapcat #(if (vector? %) 
+                   (apply range %) 
+                   [%]) 
+                args)))
+
+(def truthy #(when % true))
+(def falsey #(not (truthy %)))
+
+(defn whitelist
+  "Takes a thing with keys (i.e. maps or sets) and checks if it contains only
+   the keys listed in the given whitelist."
+  [wl things]
+  (set/subset? (set (keys things))
+               (set wl)))
+
