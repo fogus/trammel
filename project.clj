e9cab0c02af61da4ef0ad3c473cab2893c61067e
fogus <mefogus@gmail.com>
2011-05-26 16:05:07 -0400

    Bumping version to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject trammel "0.5.0"
+(defproject trammel "0.6.0-SNAPSHOT"
   :description "A library providing contracts programming for Clojure."
   :dependencies [[org.clojure/clojure "1.2.0"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
--- src/trammel/provide.clj
+++ src/trammel/provide.clj
@@ -10,7 +10,6 @@
                     (if (vector? (first more)) 
                       (list* `contract n ds more) 
                       (first more)))]
-    (println kontracts)
     `(do
        ~@(for [[n# c#] (zipmap fn-names kontracts)]
            (list `alter-var-root (list `var n#) 
