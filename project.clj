c5ca48e33177924dda5dc1d7bae77d2351d6df2e
fogus <mefogus@gmail.com>
2011-08-24 22:52:36 -0400

    Upgraded to latest 1.3 clj version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject trammel "0.6.0-SNAPSHOT"
   :description "A library providing contracts programming for Clojure."
-  :dependencies [[org.clojure/clojure "1.3.0-master-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.3.0-beta1"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
                      [jline "0.9.94"]
                      [swank-clojure "1.2.1"]
