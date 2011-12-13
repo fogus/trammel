ecb1b92796f303b0cf76d6d8538043a2d9793557
fogus <mefogus@gmail.com>
2011-12-13 07:55:01 -0500

    Release 0.6.0

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject trammel "0.6.0-SNAPSHOT"
+(defproject trammel "0.6.0"
   :description "A library providing contracts programming for Clojure."
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
                      [jline "0.9.94"]
                      [swank-clojure "1.2.1"]
-                     [marginalia "0.6.0"]
+                     [marginalia "0.6.1"]
                      [lein-marginalia "0.6.0"]])
