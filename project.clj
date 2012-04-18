af65709944f6c812fbc3cb9d0a11ddf74b3d8054
fogus <mefogus@gmail.com>
2012-04-18 08:13:16 -0400

    Upgraded to CLJ 1.4

--- project.clj
+++ project.clj
@@ -1,7 +1,5 @@
 (defproject trammel "0.8.0-SNAPSHOT"
   :description "A library providing contracts programming for Clojure."
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [thneed "1.0.0-SNAPSHOT"]]
   :dev-dependencies [[lein-clojars "0.8.0"]
                      [jline "0.9.94"]
                      [swank-clojure "1.4.0"]
@@ -11,6 +9,6 @@
                "1.2"   [[org.clojure/clojure "1.2.0"]]
                "1.2.1" [[org.clojure/clojure "1.2.1"]]
                "1.3"   [[org.clojure/clojure "1.3.0"]]
-               "1.4"   [[org.clojure/clojure "1.4.0-beta1"]]})
+               "1.4"   [[org.clojure/clojure "1.4.0"]]})
 
 ;; lein multi test
