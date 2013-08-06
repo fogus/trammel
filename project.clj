fdb4a3d88baa4b4459662d88856c6d14306ecda7
Fogus <mefogus@gmail.com>
2013-08-05 22:14:49 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject trammel "0.8.0-SNAPSHOT"
   :description "A library providing contracts programming for Clojure."
-  :dependencies [[org.clojure/clojure "1.5.0"] 
+  :dependencies [[org.clojure/clojure "1.5.1"] 
                  [org.clojure/core.contracts "0.0.2"]]
   :dev-dependencies [[lein-clojars "0.8.0"]
                      [jline "0.9.94"]
@@ -12,6 +12,6 @@
                "1.2.1" [[org.clojure/clojure "1.2.1"]]
                "1.3"   [[org.clojure/clojure "1.3.0"]]
                "1.4"   [[org.clojure/clojure "1.4.0"]]
-               "1.5"   [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]})
+               "1.5"   [[org.clojure/clojure "1.5.0"]]})
 
 ;; lein multi test
