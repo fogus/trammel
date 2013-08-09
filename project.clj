489508825ad418260e013c4f263baf7e88224ccd
Fogus <mefogus@gmail.com>
2013-08-09 09:26:56 -0400

    Moving to lein profiles

--- project.clj
+++ project.clj
@@ -2,11 +2,7 @@
   :description "A library providing contracts programming for Clojure."
   :dependencies [[org.clojure/clojure "1.5.1"] 
                  [org.clojure/core.contracts "0.0.4"]]
-  :dev-dependencies [[lein-clojars "0.8.0"]
-                     [jline "0.9.94"]
-                     [swank-clojure "1.4.2"]
-                     [lein-marginalia "0.7.1"]
-                     [lein-multi "1.1.0"]]
+  :dev-dependencies [[lein-marginalia "0.7.1"]]
   :profiles {:1.2   {:dependencies [[org.clojure/clojure "1.2.0"]]}
              :1.2.1 {:dependencies [[org.clojure/clojure "1.2.1"]]}
              :1.3   {:dependencies [[org.clojure/clojure "1.3.0"]]}
