cf3bd1b8628947b4a817315bf1b94371f5c65dff
fogus <mefogus@gmail.com>
2014-07-31 11:05:21 -0400

    Added latest core.contracts version

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject trammel "0.8.0-SNAPSHOT"
   :description "A library providing contracts programming for Clojure."
   :dependencies [[org.clojure/clojure "1.6.0"] 
-                 [org.clojure/core.contracts "0.0.4"]]
+                 [org.clojure/core.contracts "0.0.5"]]
   :dev-dependencies [[lein-marginalia "0.7.1"]]
   :profiles {:1.2   {:dependencies [[org.clojure/clojure "1.2.0"]]}
              :1.2.1 {:dependencies [[org.clojure/clojure "1.2.1"]]}
